7b739cc614a7c09269ec40a48ea15932f72f20da
joellegg <joellegg@bellsouth.net>
2017-03-03 11:47:37 -0600

    replaced asterics with stars

--- flag.js
+++ flag.js
@@ -2,15 +2,16 @@
 
 const chalk = require('chalk');
 const {red, white, bold, bgBlue, bgWhite, bgRed} = require('chalk');
-
+const star = '\u2606';
+console.log(star)
 // style a string
-console.log(bgBlue.white.bold(' *   *   *   *   *   *   * ') + bgRed('                                                 '));
-console.log(bgBlue.white.bold('   *   *   *   *   *   *   ') + bgWhite('                                                 '));
-console.log(bgBlue.white.bold(' *   *   *   *   *   *   * ') + bgRed('                                                 '));
-console.log(bgBlue.white.bold('   *   *   *   *   *   *   ') + bgWhite('                                                 '));
-console.log(bgBlue.white.bold(' *   *   *   *   *   *   * ') + bgRed('                                                 '));
-console.log(bgBlue.white.bold('   *   *   *   *   *   *   ') + bgWhite('                                                 '));
-console.log(bgBlue.white.bold(' *   *   *   *   *   *   * ') + bgRed('                                                 '));
+console.log(bgBlue.white.bold(` ${star}   ${star}   ${star}   ${star}   ${star}   ${star}   ${star} `) + bgRed('                                                 '));
+console.log(bgBlue.white.bold(`   ${star}   ${star}   ${star}   ${star}   ${star}   ${star}   `) + bgWhite('                                                 '));
+console.log(bgBlue.white.bold(` ${star}   ${star}   ${star}   ${star}   ${star}   ${star}   ${star} `) + bgRed('                                                 '));
+console.log(bgBlue.white.bold(`   ${star}   ${star}   ${star}   ${star}   ${star}   ${star}   `) + bgWhite('                                                 '));
+console.log(bgBlue.white.bold(` ${star}   ${star}   ${star}   ${star}   ${star}   ${star}   ${star} `) + bgRed('                                                 '));
+console.log(bgBlue.white.bold(`   ${star}   ${star}   ${star}   ${star}   ${star}   ${star}   `) + bgWhite('                                                 '));
+console.log(bgBlue.white.bold(` ${star}   ${star}   ${star}   ${star}   ${star}   ${star}   ${star} `) + bgRed('                                                 '));
 console.log(bgWhite('                                                                            '));
 console.log(bgRed('                                                                            '));
 console.log(bgWhite('                                                                            '));
