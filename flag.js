f159721e315721aacd75e92b8e79fb6d0edd004d
joellegg <joellegg@bellsouth.net>
2017-03-03 14:05:24 -0600

    changed star spacing

--- flag.js
+++ flag.js
@@ -4,18 +4,17 @@ const chalk = require('chalk');
 const {red, white, bold, bgBlue, bgWhite, bgRed} = require('chalk');
 const star = '\u2606';
 const starMargin = ' ';
-const starMarginRight = '  ';
-const starPadding = '   ';
+const starPadding = '  ';
 
 
 for (i = 0; i < 13; i++) {
   if (i % 2 === 0 && i < 7) {
-    console.log(bgBlue.white.bold(`${starMargin}${star}${starPadding}${star}${starPadding}${star}${starPadding}${star}${starPadding}${star}${starPadding}${star}${starPadding}${star}${starMarginRight}`) + bgRed('                                                 '));
+    console.log(bgBlue.white.bold(`${starMargin}${star}${starMargin}${star}${starMargin}${star}${starMargin}${star}${starMargin}${star}${starMargin}${star}${starMargin}${star}${starMargin}`) + bgRed('                                                 '));
   } else if (i % 2 === 1 && i < 7) {
-    console.log(bgBlue.white.bold(`${starPadding}${star}   ${star}   ${star}   ${star}   ${star}   ${star}${starPadding}${starMargin}`) + bgWhite('                                                 '));
+    console.log(bgBlue.white.bold(`${starPadding}${star}${starMargin}${star}${starMargin}${star}${starMargin}${star}${starMargin}${star}${starMargin}${star}${starPadding}`) + bgWhite('                                                 '));
   } else if (i % 2 === 1 && i > 6) {
-    console.log(bgWhite('                                                                             '));
+    console.log(bgWhite('                                                                '));
   } else if (i % 2 === 0 && i > 6) {
-    console.log(bgRed('                                                                             '));
+    console.log(bgRed('                                                                '));
   }
 }
