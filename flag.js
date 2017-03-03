8562f5e5a76173b81cdd8eb7aab3c32d87adc276
joellegg <joellegg@bellsouth.net>
2017-03-03 12:06:17 -0600

    added some right padding to the stars

--- flag.js
+++ flag.js
@@ -3,16 +3,19 @@
 const chalk = require('chalk');
 const {red, white, bold, bgBlue, bgWhite, bgRed} = require('chalk');
 const star = '\u2606';
+const starMargin = ' ';
+const starMarginRight = '  ';
+const starPadding = '   ';
 
 
 for (i = 0; i < 13; i++) {
   if (i % 2 === 0 && i < 7) {
-    console.log(bgBlue.white.bold(` ${star}   ${star}   ${star}   ${star}   ${star}   ${star}   ${star} `) + bgRed('                                                 '));
+    console.log(bgBlue.white.bold(`${starMargin}${star}${starPadding}${star}${starPadding}${star}${starPadding}${star}${starPadding}${star}${starPadding}${star}${starPadding}${star}${starMarginRight}`) + bgRed('                                                 '));
   } else if (i % 2 === 1 && i < 7) {
-    console.log(bgBlue.white.bold(`   ${star}   ${star}   ${star}   ${star}   ${star}   ${star}   `) + bgWhite('                                                 '));
+    console.log(bgBlue.white.bold(`${starPadding}${star}   ${star}   ${star}   ${star}   ${star}   ${star}${starPadding}${starMargin}`) + bgWhite('                                                 '));
   } else if (i % 2 === 1 && i > 6) {
-    console.log(bgWhite('                                                                            '));
+    console.log(bgWhite('                                                                             '));
   } else if (i % 2 === 0 && i > 6) {
-    console.log(bgRed('                                                                            '));
+    console.log(bgRed('                                                                             '));
   }
 }
