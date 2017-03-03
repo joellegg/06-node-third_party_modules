5cb12557aa82498f59566b6239aa45437f6e265a
joellegg <joellegg@bellsouth.net>
2017-03-02 19:44:03 -0600

    stars print with blue background

--- flag.js
+++ flag.js
@@ -1,1 +1,12 @@
 #!/usr/bin/env node
+
+var chalk = require('chalk');
+
+// style a string
+console.log(chalk.white.bgBlue(' * * * * * * * '));
+console.log(chalk.white.bgBlue('  * * * * * *  '));
+console.log(chalk.white.bgBlue(' * * * * * * * '));
+console.log(chalk.white.bgBlue('  * * * * * *  '));
+console.log(chalk.white.bgBlue(' * * * * * * * '));
+console.log(chalk.white.bgBlue('  * * * * * *  '));
+console.log(chalk.white.bgBlue(' * * * * * * * '));
